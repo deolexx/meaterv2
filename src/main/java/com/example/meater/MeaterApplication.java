a34a8e8d977972bad67d9c13e3d52dce5d627c32
Serhii Lukianenko <lukyanenko88@gmail.com>
2021-09-12 00:20:01 +0300

    Initial commit for the app ** Created with spring boot starter ** spring-web thymeleaf ** dependencies added

---
+++ src/main/java/com/example/meater/MeaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.meater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeaterApplication.class, args);
+    }
+
+}
